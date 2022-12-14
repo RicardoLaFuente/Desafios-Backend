1985dc3dd788a4b27257a131316ec45a50231b0e
Ricardo La Fuente <dario64_88@hotmail.com.ar>
2022-12-14 18:24:37 -0300

    repaso de validacion de campos  y ID

--- index.js
+++ index.js
@@ -17,22 +17,14 @@ class ProductManager {
         if (existe) {
             return console.log("El CODIGO  " + product.code + " Ya existe");
         }
-        if (!product.title, !product.description) {
+        if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock) {
             return console.log("Falta llenar un campo")
-        }
-      
-        else {
-            if (!product.title & !product.description) {
-                return console.log("Falta llenar un campo")
-    
-            } else {
-            this.products.push(product);}
         }
 
-         /* asi no lo pude hacer funcionar, solo poniendo dos campos.. con && tampoco funciona
-          if (!product.title, !product.description, !product.price, !product.thumbnail, !product.code, !product.stock) {
-            return console.log("Falta llenar un campo")
-        }*/
+        else {
+            this.products.push(product);
+            ProductManager.id++;
+        }
     }
 
     getProducts() {
@@ -55,18 +47,18 @@ class ProductManager {
 const nuevosProductos = new ProductManager();
 
 const product1 = {
-     title: "Cheescake",
+    title: "Cheescake",
     description: "Torta Clasica",
     price: 2500,
     thumbnail: "ABCD",
-    code: "120", 
+    code: "120",
     stock: 10,
 
 };
 
 const product2 = {
-   title: "Chocotorta",
-   // description: "Torta Clasica",     *anulo para forzar error
+    title: "Chocotorta",
+    // description: "Torta Clasica",   //  *anulo para forzar error
     price: 2700,
     thumbnail: "ABCDE",
     code: "121",
@@ -78,16 +70,26 @@ const product3 = {
     title: "Marquise",
     description: "Torta Clasica",
     price: 3100,
-    thumbnail: "ABCDEF",
+    thumbnail: "ABCDEFG",
     code: "120", //repite code para forzar error
     stock: 15,
+};
+
+const product4 = {
+    title: "Chocoreo",
+    description: "Torta Clasica",
+    price: 3300,
+    thumbnail: "ABCDEFGH",
+    code: "123",
+    stock: 10,
 };
 
 nuevosProductos.addProduct(product1);
 nuevosProductos.addProduct(product2);
 nuevosProductos.addProduct(product3);
-
+nuevosProductos.addProduct(product4);
 
 console.log(nuevosProductos.getProducts())
 
 console.log("hola mundo")
+console.log(nuevosProductos.getProductsById(10))
