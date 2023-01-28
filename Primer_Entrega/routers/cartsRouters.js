c9842abab492478d6b512667e5fca208e0ebba35
Ricardo La Fuente <dario64_88@hotmail.com.ar>
2023-01-28 20:18:30 -0300

    casi listo pre-entrega, falta stock

--- Primer_Entrega/routers/cartsRouters.js
+++ Primer_Entrega/routers/cartsRouters.js
@@ -2,7 +2,7 @@ const express = require("express");
 const routerCarts = express.Router();
 const fs = require("fs");
 
-let cartBase = JSON.parse(fs.readFileSync("./database/carritos.json", "utf-8"));
+const cartBase = JSON.parse(fs.readFileSync("./database/carritos.json", "utf-8"));
 let productCarrito = JSON.parse(fs.readFileSync("./database/productos.json", "utf-8"));
 
 
@@ -18,7 +18,7 @@ const generadorID = (array) => {
     return id;//retornamos el id
 };
 
-console.log(cartBase)
+console.log("cartBase", cartBase)
 routerCarts.post("/", (req, res) => {
 
     const products = [];
@@ -39,54 +39,30 @@ routerCarts.get("/:cid", (req, res) => {
 
 routerCarts.post("/:cid/product/:pid", (req, res) => {
     const {cid} = req.params;
-    const idcart = cartBase.find((pro => pro.id = cid));
-    console.log("idCart", idcart)
+    const idcart = cartBase.find((pro => pro.id === +cid));
+   
   
     const {pid} = req.params;
-    const idpro = productCarrito.find((pro => pro.id = pid));
-    console.log("idpro", idpro.products)
-    //const proId = idpro.id
+    const idpro = productCarrito.find((pro => pro.id === +pid));
+   
+   
 
-    const productoRepetido = idcart.productCarrito.find( e => e.id = pid);
+   
+    const productoRepetido = idcart.products.find( e => e.id === +pid);
+    
 
     if(productoRepetido){
-        idcart.productCarrito.map( (element)=>{ if(element.id = pid){element.quantity++}})
+        idcart.products.map( (element)=>{ if(element.id === +pid){element.quantity++}})
     } else {
-        idcart.productCarrito.push({
+        idcart.products.push({
             id: idpro.id,
             quantity: 1
         })
     }
- 
+   
     fs.writeFileSync("./database/carritos.json", JSON.stringify(cartBase),(err)=>{ throw new Error (err)});
     
     res.send(idcart);
-
-  /*const productNew = {
-    id:proId,
-    quantity: 1
-  }
-  carritoId.push(productNew)
-  const checkInCart = cartBase.find(p => p.id === carritoId)
-  console.log("checking", checkInCart)
-  if(checkInCart) {
-    const verify1 = checkInCart.products.find(product=>product.id===proId)
-  
-    if(verify1){
-        quantity++
-        
-    }else{
-        carritoId.push(productNew)
-    }
-  }
-
-  fs.writeFileSync("./database/carritos.json", JSON.stringify(cartBase));
-    
-    const buscarCarrito=carritoId.find((pro) => pro.product === +pid);
-
-  
-    res.send(idcart);
-    // agregar el producto al carrito */
 });
 
 
